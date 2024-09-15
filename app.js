f931975f419c35dc37e417d2e26f43afea1a31aa
Tej749 <jargha261975@gmail.com>
2024-09-15 09:05:43 +0545

    update

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 const express = require("express");
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hellow World ...Home Page...");
+});
+
+app.get("/about", (req, res) => {
+  res.send("Welcome to About pages....");
+});
+
+app.listen(3000, () => {
+  console.log("Huricane Project is started....");
+});
